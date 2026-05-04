d4443533105d014b4e5c0f9f5847a3d608341b41
pankajcoder28 <pankajpenu147@gmail.com>
2026-05-04 13:39:47 +0530

    fix: define PORT variable to set server listening port

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ connectDB();
 
 
 
-const PORT;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
 });
